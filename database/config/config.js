4a2e87a1f1e10950805e99982242224b56313b11
Matelavan <lavandeiram@udesa.edu.ar>
2024-11-16 20:58:53 -0300

    agregue la base de datos y el sequelize

---
+++ database/config/config.js
@@ -0,0 +1,12 @@
+let config = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "ecommerce",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  
+}
+
+module.exports = config;
