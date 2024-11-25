54f8b39e932b1104b92de4e37a43be24f0697968
matiasroyann <matiasroyan11@gmail.com>
2024-11-25 08:33:34 -0300

    coreccion ejs detalle

--- database/config/config.js
+++ database/config/config.js
@@ -4,8 +4,8 @@ let config = {
     "password": "root",       
     "database": "ecommerce",   
     "host": "127.0.0.1",       
-    "dialect": "mysql",
-    "port": "3306"            
+    "dialect": "mysql"
+              
   }
 };
 
