0b058ca8ee95f565e2ba19fe65702b5fe02bd9c9
reygianfranco <grey@udesa.edu.ar>
2024-11-24 21:09:22 -0300

    productcontroller, y rutas falta el ejs, de buscar producto y de product add

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,22 +7,27 @@ const productoController = {
         return res.render("product-add");
     },
 
-    search: function (req, res) {
-        let qs = req.query.Product;
+    add: function(req, res){
 
-        let filtrado = {
-            where: [{ title: { [op.like]: `%${qs}%` } }],
-        };
-
-        db.Product.findAll(filtrado)
-            .then(function (results) {
-                return res.send(results); 
-            })
-            .catch(function (err) {
-                console.log(err); 
-            });
+        if (req.session.user != undefined) {
+            return res.render("product-add", {title: "Agregar Producto"})
+        } else {
+            return res.redirect("/users/login")
+        }
     },
 
+    processProduct: function (req, res) {
+            if (req.body.title == "") {
+             return res.send('El título no puede estar vacío.');
+        }
+            if (req.body.description == "") {
+                return res.send('La descripción no puede estar vacía.');
+        }
+            if (req.body.image == "") {
+                return res.send('La imagen no puede estar vacía.');
+        }
+          
+    },
 
     store: function (req, res) {
         let producto = req.body;
@@ -35,6 +40,62 @@ const productoController = {
                 console.log(err); 
             });
     },
+
+    search: function (req, res) {
+        let qs = req.query.Product;
+
+        let filtrado = {
+            where: [{ title: { [op.like]: `%${qs}%` } }],
+            order: [['createdAt', 'DESC']],
+            include: [{ model: db.User, as: 'user' }]
+        };
+
+        db.Product.findAll(filtrado)
+            .then(function (results) {
+                if (results.length > 0) {
+                    return res.render('search-results', { results: results });
+                } else {
+                    return res.send('No hay resultados para su criterio de búsqueda');
+                }
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    },
+
+    showProductDetail: function (req, res) {
+        let productId = req.params.id;
+
+        db.Product.findByPk(productId, {
+            include: [{ model: db.User, as: 'user' }]
+        })
+            .then(function (product) {
+                if (product) {
+                    return res.render('product', { product: product });
+                } else {
+                    return res.send('Producto no encontrado');
+                }
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    },
+
+    showUserProfile : function (req, res) {
+        let userId = req.params.id;
+
+        db.User.findByPk(userId)
+            .then(function (user) {
+                if (user) {
+                    return res.render('user-profile', { user: user });
+                } else {
+                    return res.send('Usuario no encontrado');
+                }
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
 };
 
 module.exports = productoController;
--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.get('/products', productController.showFormCreate);
-router.post('/products', productController.store)
+router.get('/', productController.showFormCreate);
+
+router.get('/add', productController.add);
+router.post('/add', productController.processProduct);
+router.post('/store', productController.store);
+
+router.get('/search', productController.search);
+router.post('/search', productController.showProductDetail);
+router.get('/id:/id', productController.showUserProfile);
+
 
 module.exports = router
